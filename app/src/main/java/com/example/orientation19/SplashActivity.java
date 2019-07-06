0894275acdb967f221dbbfaf88feb91dda789890
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2019-07-06 22:40:29 +0530

    Added Splash Screen

---
+++ app/src/main/java/com/example/orientation19/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.orientation19;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.wangyuwei.particleview.ParticleView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ParticleView particleView = findViewById(R.id.particle_view);
+        particleView.startAnim();
+        particleView.setOnParticleAnimListener(new ParticleView.ParticleAnimListener() {
+            @Override
+            public void onAnimationEnd() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
